93a6fbfb83d0613a8796c7e06f8564384339094a
Nguyen Huu Phuoc <huuphuoc.96.7.29@gmail.com>
2023-04-19 23:22:40 +0700

    Update gitignore.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nguyen Huu Phhuoc
         </a>
       </header>
     </div>
