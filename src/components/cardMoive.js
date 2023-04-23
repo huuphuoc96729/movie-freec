c1e8d843aeaa38c057fb302495c4ad6d45ca8579
Nguyen Huu Phuoc <huuphuoc.96.7.29@gmail.com>
2023-04-23 13:38:50 +0700

    Update style.

--- src/components/cardMoive.js
+++ src/components/cardMoive.js
@@ -4,34 +4,28 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
 class CardMovie extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     const {data} = this.props;
     const {Type, Title, Year, Poster} = data;
     return (
-      <>
-      <div className="type">
-        {Type.charAt(0).toUpperCase() + Type.slice(1)}
+      <div className="card-movie">
+        <div className="type">
+          {Type.charAt(0).toUpperCase() + Type.slice(1)}
+        </div>
+        <Card className='text-center' sx={{height: '360px'}}>
+          <CardMedia
+            component="img"
+            alt=""
+            height="300px"
+            image={Poster}
+          />
+          <CardContent className='p-2'>
+            <div className="title mt-1 mb-1">{Title}</div>
+            <div className="year mb-1">({Year})</div>
+          </CardContent>
+        </Card>
       </div>
-      <Card className='text-center' sx={{height: '360px'}}>
-        <CardMedia
-          component="img"
-          alt=""
-          height="300px"
-          image={Poster}
-           // style={{
-           //   width: 'auto', margin: '0 auto'
-           // }}
-        />
-        <CardContent className='p-2'>
-          <div className="title pt-1 pb-1">{Title}</div>
-          <div className="year pb-1">({Year})</div>
-        </CardContent>
-      </Card>
-      </>
     )
   }
 }
